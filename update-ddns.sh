8255b5a83def3be8cd00763e4d7d1c3ccc36c718
Alex <alex@alexfell.co.uk>
2021-09-10 11:54:18 +0100

    Fixed hostnames

--- update-ddns.sh
+++ update-ddns.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -n -X POST https://ipv4.api.mythic-beasts.com/dns/v2/dynamic/alexfell.co.uk
-curl -n -X POST https://ipv4.api.mythic-beasts.com/dns/v2/dynamic/fella.alexfell.co.uk
+curl -n -X POST https://ipv4.api.mythic-beasts.com/dns/v2/dynamic/example.com
+curl -n -X POST https://ipv4.api.mythic-beasts.com/dns/v2/dynamic/host.axample.com
